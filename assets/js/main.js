537a049f7c693159d43bd0a236112dd74fe81c52
qimmah-ksa <abdullass@gmail.com>
2026-03-12 22:34:18 +0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RNF0DYEEFT');
+</script>
+    
+  <script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "قمة التميز للاستشارات المالية والإدارية",
+  "url": "https://www.gimahpro.com/",
+  "logo": "https://www.gimahpro.com/assets/images/gimahpro-logo.png",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+966533221169",
+    "contactType": "customer service",
+    "areaServed": "SA",
+    "availableLanguage": "Arabic"
+  },
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "حي الياسمين",
+    "addressLocality": "الرياض",
+    "postalCode": "13325",
+    "addressCountry": "SA"
+  },
+  "sameAs": [
+    "https://www.linkedin.com/company/gimahpro",
+    "https://twitter.com/gimahpro",
+    "https://www.instagram.com/gimahpro"
+  ]
+}
+</script>
