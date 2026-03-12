3e1d5227b15e77721c78a169279764d1d744cb05
qimmah-ksa <abdullass@gmail.com>
2026-03-12 23:01:44 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,3 @@
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-RNF0DYEEFT');
-</script>
-    
-  <script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "قمة التميز للاستشارات المالية والإدارية",
-  "url": "https://www.gimahpro.com/",
-  "logo": "https://www.gimahpro.com/assets/images/gimahpro-logo.png",
-  "contactPoint": {
-    "@type": "ContactPoint",
-    "telephone": "+966533221169",
-    "contactType": "customer service",
-    "areaServed": "SA",
-    "availableLanguage": "Arabic"
-  },
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "حي الياسمين",
-    "addressLocality": "الرياض",
-    "postalCode": "13325",
-    "addressCountry": "SA"
-  },
-  "sameAs": [
-    "https://www.linkedin.com/company/gimahpro",
-    "https://twitter.com/gimahpro",
-    "https://www.instagram.com/gimahpro"
-  ]
-}
-</script>
-<script>
 document.getElementById("contact-form").addEventListener("submit", async function(e) {
     e.preventDefault();
 
@@ -55,4 +18,3 @@ document.getElementById("contact-form").addEventListener("submit", async functio
         document.getElementById("form-error").style.display = "block";
     }
 });
-</script>
